14e91f6607f02eaa9bf27f20c25fee4723463795
AdonaiHernandez <adonai.acsii@gmail.com>
2020-03-13 14:42:03 +0000

    Create suma.s

---
+++ suma.s
@@ -0,0 +1,41 @@
+section .bss
+    num1 resb 1
+    num2 resb 1
+    sum resb 2
+
+section .text
+
+global _start
+
+_start:
+
+    mov byte [num1], '1'
+    mov byte [num2], '2'
+
+    mov al, [num1]
+    mov bl, [num2]
+    sub al, '0'
+    sub bl, '0'
+    add al, bl
+    
+    ;mov ah, 0xA
+    ;add al, ah
+
+    add al, '0'
+
+    mov [sum], al
+
+    mov edx, 2
+    mov ecx, sum
+
+    call print_int
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+print_int:
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+    ret
